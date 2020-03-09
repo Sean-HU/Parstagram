fec378114c3fec25b8ed8594d8d53591b7cfc8e9
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2020-03-09 18:41:09 -0400

    Added pull to refresh

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -16,28 +16,46 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var posts = [PFObject]()
     
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        loadPosts()
+    }
+    
+    @objc func loadPosts() {
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
         query.limit = 20
-        
+       
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
                 self.posts = posts!
                 self.tableView.reloadData()
             }
+       }
+        
+        run(after: 2) {
+           self.refreshControl.endRefreshing()
         }
+    }
+    
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
